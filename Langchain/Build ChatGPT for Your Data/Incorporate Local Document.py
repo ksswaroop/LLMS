2b541f4da073ab85722d8427630a8cd243b79af6
Swaroop Kottapalli <k.saiswaroop@hotmail.com>
2024-09-05 01:17:56 +0530

    "Commit changes related to function"

--- Langchain/Build ChatGPT for Your Data/Incorporate Local Document.py
+++ Langchain/Build ChatGPT for Your Data/Incorporate Local Document.py
@@ -18,47 +18,51 @@ embeddings=OpenAIEmbeddings()
 with open("/home/swaroop/Documents/Coding/LLMS/Langchain/Build ChatGPT for Your Data/Alice_1.txt") as f:
     alice_in_wonderland=f.read()
 
-#print(alice_in_wonderland)
+print(alice_in_wonderland)
 
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separator = "\n")
+
 texts=text_splitter.split_text(alice_in_wonderland)
 docsearch = Chroma.from_texts(texts, embeddings)
+
 query = "What is the Rabbit late for?"
+
 docs = docsearch.similarity_search(query,k=1)
 
 #########
 
-CHUNK_SIZE = 1000
-CHUNK_OVERLAP = 200
-K = 3  
+# CHUNK_SIZE = 1000
+# CHUNK_OVERLAP = 200
+# K = 3  
 
 
-def create_embedding_model():
-    """Creates an instance of the OpenAI embedding model."""
-    return OpenAIEmbeddings()
+# def create_embedding_model():
+#     """Creates an instance of the OpenAI embedding model."""
+#     return OpenAIEmbeddings()
 
-file_path="/home/swaroop/Documents/Coding/LLMS/Langchain/Build ChatGPT for Your Data/Alice_1.txt")
+# file_path="/home/swaroop/Documents/Coding/LLMS/Langchain/Build ChatGPT for Your Data/Alice_1.txt")
 
-def create_vector_database(file_path):
-    """
-    Creates a FAISS vector database from the provided PDF reader object.
+# def create_vector_database(file_path):
+#     """
+#     Creates a FAISS vector database from the provided PDF reader object.
 
-    Args:
-        pdf_reader (PyPDF2.PdfReader): The PDF reader object containing the document content.
+#     Args:
+#         pdf_reader (PyPDF2.PdfReader): The PDF reader object containing the document content.
 
-    Returns:
-        FAISS: The created FAISS vector database.
-    """
-    with open(file_path)as f:
-        texts=f.read()
+#     Returns:
+#         FAISS: The created FAISS vector database.
+#     """
+#     with open(file_path)as f:
+#         texts=f.read()
 
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=CHUNK_SIZE,
-        chunk_overlap=CHUNK_OVERLAP,
-        length_function=len
-    )
+#     text_splitter = RecursiveCharacterTextSplitter(
+#         chunk_size=CHUNK_SIZE,
+#         chunk_overlap=CHUNK_OVERLAP,
+#         length_function=len
+#     )
 
-    chunks = text_splitter.split_text(texts)
-    db = Chroma.from_texts(chunks, embedding=create_embedding_model())
-    return db
+#     chunks = text_splitter.split_text(texts)
+#     db = Chroma.from_texts(chunks, embedding=create_embedding_model())
+#     return db
+
