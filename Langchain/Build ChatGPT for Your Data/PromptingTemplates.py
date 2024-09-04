4f994b7aaac3d52733f5f37965a07af52b5fb6c8
Swaroop Kottapalli <k.saiswaroop@hotmail.com>
2024-09-04 13:25:47 +0530

    Updated the variables

--- Langchain/Build ChatGPT for Your Data/PromptingTemplates.py
+++ Langchain/Build ChatGPT for Your Data/PromptingTemplates.py
@@ -11,15 +11,15 @@ def disp_markdown(text:str)-> None:
 chat_model=ChatOpenAI(model_name="gpt-3.5-turbo")
 
 # We can signift variables we want access to by wrapping them in {}
-#system_prompt_template="You are an expert in {SUBJECT}, and you're currently feeling {MOOD}"
-system_prompt_template=SystemMessagePromptTemplate.from_template("You are an expert in {SUBJECT}, and you're currently feeling {MOOD}")
-#user_prompt_template="{CONTENT}"
-user_prompt_template=HumanMessagePromptTemplate.from_template("{CONTENT}")
+system_prompt_template="You are an expert in {SUBJECT}, and you're currently feeling {MOOD}"
+system_prompt_template=SystemMessagePromptTemplate.from_template(system_prompt_template)
+user_prompt_template="{CONTENT}"
+user_prompt_template=HumanMessagePromptTemplate.from_template(user_prompt_template)
 
 # put them together into ChatPromptTemplate
 chat_prompt=ChatPromptTemplate.from_messages([system_prompt_template,user_prompt_template])
 # Note the method "to_messages()", that's what converts formatted prompt into 
 formatted_chat_prompt= chat_prompt.format_prompt(SUBJECT="Sparkling waters",MOOD="Joyful",CONTENT="Is Bubly a good sparkling water?").to_messages()
 
-#disp_markdown(chat_model.invoke(messages))
+#disp_markdown(chat_model.invoke(formatted_chat_prompt))
 print(chat_model.invoke(formatted_chat_prompt).content)
